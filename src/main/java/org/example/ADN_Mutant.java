2eb609c7d4a279879f26205d03aac28432384fc7
gjohnn <juansgb316@outlook.com>
2024-10-07 00:14:44 -0300

    Optimizacion comparando el 1ro con el 4to

--- src/main/java/org/example/ADN_Mutant.java
+++ src/main/java/org/example/ADN_Mutant.java
@@ -1,90 +1,66 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.logging.Logger;
+
 
 public class ADN_Mutant {
     public static int MIN = 4;
     public static String[] dna;
     static final Logger logger = Logger.getLogger("ADN_Mutant");
     static char index;
-    public static String[] dnaX = new String[] {
-            "XXXXXXX",
-            "XXXXXXX",
-            "XXXXXXX",
-            "XXXXXXX",
-            "XXXXXXX",
-            "XXXXXXX",
-            "XXXXXXX"
-    };
+    static char indexFinal;
+    static int rows;
+    static int columns;
 
     public static void main(String[] args) {
-        logger.info("ADN MUTANT - STARTED");
-        dna = new String[] {
-                "AACCGCT",
-                "ACAATCT",
-                "AAGTGAA",
-                "AGATGGC",
-                "TCGCTTA",
-                "ATAGTTA",
-                "TCTCTGC"
+        long startTime = System.nanoTime();
+//
+//        dna = new String[]{
+//                "AACCGCT",
+//                "ACACCGT",
+//                "CAGTGAA",
+//                "AGAGTGC",
+//                "TCGGATA",
+//                "ATACTGT",
+//                "TCTCTGG"
+//        };
+       dna = new String[]{
+                "ATGCGTACGTACGTA",
+                "CGTACGTACGTACGA",
+                "TACGTACGTACTGAC",
+                "GTACTGACGTACGTA",
+                "CGTACGTACGTACGA",
+                "TACGTACGTACTGAC",
+                "GTACTGACGTACGTA",
+                "ATGCGTACGTACGTA",
+                "CGTACGTACGTACGA",
+                "TACGTACGTACTGAC",
+                "GTACTGACGTACGTA",
+                "AATACTGACGTACGA", // Coincidencia en la columna 1 (cuatro 'A')
+                "AAGACTGACGTACGA",
+                "AAGACTGACGTACGA",
+                "AACACTGACGTACGA"
         };
-        isMutant();
-        logger.info("ADN MUTANT - FINISHED");
+
+        rows = dna.length - 3;
+        columns = dna.length - 3;
+        if (isMutant()) {
+            System.out.println("Es mutante");
+        }else{
+            System.out.println("NO es mutante");
+        }
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("El código tomó " + duration + " milisegundos en ejecutarse.");
 
     }
 
-    public static void isMutant() {
-        ArrayList<String> causas = new ArrayList<>();
-        if (checkRows(dna)) {
-            causas.add("Fila");
-        }
-        if (checkColumns(transponerDNA(dna))) {
-            causas.add("Columna");
-        }
-        if (checkDiagonals()) {
-            causas.add("Diagonal");
-        }
-        if (checkInverseDiagonals()) {
-            causas.add("Diagonal inversa");
-        }
-        if (!causas.isEmpty()) {
-            System.out.println("Es mutante por: ");
-            for (String causa : causas) {
-                System.out.println(causa);
-            }
-        } else
-            logger.info("No es mutante.");
+    public static boolean isMutant() {
+        return checkRows() || checkColumns() || checkDiagonals() || checkInverseDiagonals();
     }
 
-    public static String[] transponerDNA(String[] dna) {
-        int columnas = dna[0].length();
 
-        // Crear un arreglo de StringBuilder para construir las nuevas cadenas
-        StringBuilder[] dnaTranspuesto = new StringBuilder[columnas];
-
-        // Inicializar los StringBuilder
-        for (int i = 0; i < columnas; i++) {
-            dnaTranspuesto[i] = new StringBuilder();
-        }
-
-        // Transponer el arreglo
-        for (String s : dna) {
-            for (int j = 0; j < columnas; j++) {
-                dnaTranspuesto[j].append(s.charAt(j));
-            }
-        }
-
-        // Convertir los StringBuilder a String
-        String[] resultado = new String[columnas];
-        for (int i = 0; i < columnas; i++) {
-            resultado[i] = dnaTranspuesto[i].toString();
-        }
-
-        return resultado;
-    }
-
-    private static boolean checkRows(String[] dna) {
+    private static boolean checkRows() {
 
         for (String fila : dna) {
             if (checkSequence(fila)) {
@@ -94,34 +70,69 @@ public class ADN_Mutant {
         return false;
     }
 
-    private static boolean checkColumns(String[] dna) {
-        for (String column : dna) {
-            if (checkSequence(column)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     private static boolean checkSequence(String sequence) {
         for (int i = 0; i <= sequence.length() - MIN; i++) {
-            index = sequence.charAt(i);
-            if (index == sequence.charAt(i + 1) &&
-                    index == sequence.charAt(i + 2) &&
-                    index == sequence.charAt(i + 3)) {
-                return true;
+            char firstChar = sequence.charAt(i);
+            char fourthChar = sequence.charAt(i + 3);
+            if (firstChar == fourthChar) {
+                if (firstChar == sequence.charAt(i + 1) &&
+                        firstChar == sequence.charAt(i + 2)) {
+                    System.out.println(firstChar);
+                    System.out.println(sequence.charAt(i + 1));
+                    System.out.println(sequence.charAt(i + 2));
+                    System.out.println(fourthChar);
+                    System.out.println("Mutante por fila");
+                    return true;
+                }
             }
         }
         return false;
     }
 
+    public static boolean checkColumns() {
+        for (int col = 0; col < columns; col++) {
+            for (int row = 0; row < rows; row++) {
+                char firstChar = dna[row].charAt(col);
+                char fourthChar = dna[row + 3].charAt(col);
+                if (firstChar == fourthChar) {
+                    if (firstChar == dna[row + 1].charAt(col) &&
+                            firstChar == dna[row + 2].charAt(col)) {
+                        System.out.println(firstChar);
+                        System.out.println(dna[row + 1].charAt(col));
+                        System.out.println(dna[row + 2].charAt(col));
+                        System.out.println(fourthChar);
+                        System.out.println("Mutante por columna");
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
     private static boolean checkDiagonals() {
-        for (int row = 0; row < dna[0].length() - 3; row++) {
-            for (int column = 0; column < dna[0].length() - 3; column++) {
-                index = dna[row].charAt(column);
-                if (index == dna[row + 1].charAt(column + 1) && index == dna[row + 2].charAt(column + 2)
-                        && index == dna[row + 3].charAt(column + 3)) {
-                    return true;
+        dna = new String[]{
+                "AACCGCT",
+                "ACACCGT",
+                "CAGTGAA",
+                "AGAAAGC",
+                "TCGGATA",
+                "ATACTGT",
+                "TCTCTGG"
+        };
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                char firstChar = dna[row].charAt(column);
+                char fourthChar = dna[row + 3].charAt(column + 3);
+                if (firstChar == fourthChar) {
+                    if (firstChar == dna[row + 1].charAt(column + 1) && firstChar == dna[row + 2].charAt(column + 2)) {
+                        System.out.println(firstChar);
+                        System.out.println(fourthChar);
+                        System.out.println("Mutante por diagonal");
+                        return true;
+                    }
+
                 }
             }
         }
@@ -132,11 +143,17 @@ public class ADN_Mutant {
     private static boolean checkInverseDiagonals() {
         for (int row = 0; row < dna.length - 3; row++) {
             for (int column = dna[0].length() - 1; column > 2; column--) {
-                index = dna[row].charAt(column);
-                if (index == dna[row + 1].charAt(column - 1) &&
-                        index == dna[row + 2].charAt(column - 2) &&
-                        index == dna[row + 3].charAt(column - 3)) {
-                    return true;
+                char firstChar = dna[row].charAt(column);
+                char fourthChar = dna[row + 3].charAt(column - 3);
+                if (firstChar == fourthChar) {
+                    if (firstChar == dna[row + 1].charAt(column - 1) && firstChar == dna[row + 2].charAt(column - 2)) {
+                        System.out.println(firstChar);
+                        System.out.println(dna[row + 1].charAt(column - 1));
+                        System.out.println(dna[row + 2].charAt(column - 2));
+                        System.out.println(fourthChar);
+                        System.out.println("Mutante por diagonal inversa");
+                        return true;
+                    }
                 }
             }
         }
